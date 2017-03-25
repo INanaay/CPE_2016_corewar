1fd6194f040c2c8b43f5607a5f1dc955e12ff9bd
flavian gontier <flavian.gontier@epitech.eu>
2017-03-25 12:09:15 +0100

    [bytecode] Header reading added

--- src/bytecode/reader.c
+++ src/bytecode/reader.c
@@ -5,7 +5,34 @@
 ** Login   <flavian.gontier@epitech.eu@epitech.net>
 ** 
 ** Started on  Sat Mar 25 09:36:10 2017 flavian gontier
-** Last update Sat Mar 25 09:37:17 2017 flavian gontier
+** Last update Sat Mar 25 12:08:16 2017 flavian gontier
 */
 
+#include "bytecode.h"
+#include "libmy.h"
 
+int	read_header(int fd, t_header *header)
+{
+  int	bytes;
+
+  if (fd < 0)
+    return (EXIT_ERROR);
+  if (header == NULL)
+    return (EXIT_ERROR);
+  bytes = read(fd, header, sizeof(header));
+  if (bytes <= 0)
+    return (EXIT_ERROR);
+  return (EXIT_SUCCESS);
+}
+
+int	read_instruction_id(int fd, int *id)
+{
+  int		bytes;
+
+  if (fd <= 0)
+    return (EXIT_ERROR);
+  bytes = read(fd, id, sizeof(int8_t));
+  if (bytes <= 0)
+    return (EXIT_ERROR);
+  return (EXIT_SUCCESS);
+}
