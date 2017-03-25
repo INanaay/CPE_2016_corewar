ce139b34108d8f354a35b23bdb1183ae97b8b626
flavian gontier <flavian.gontier@epitech.eu>
2017-03-25 11:40:31 +0100

    [bytecode] add bytecode's reader

---
+++ src/bytecode/reader.c
@@ -0,0 +1,11 @@
+/*
+** reader.c for corewar in /home/flavian.gontier/Tek1/C_Prog_Elem/CPE_2016_corewar/src/vm/bytecode
+** 
+** Made by flavian gontier
+** Login   <flavian.gontier@epitech.eu@epitech.net>
+** 
+** Started on  Sat Mar 25 09:36:10 2017 flavian gontier
+** Last update Sat Mar 25 09:37:17 2017 flavian gontier
+*/
+
+
