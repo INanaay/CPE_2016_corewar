87d6762a4dc857a72d11f39bd3010fc121a260ca
Nathan Schwarz <nathan.schwarz@epitech.eu>
2017-03-27 01:50:48 +0200

    sort elems done need to parse them

--- src/asm/parser.c
+++ src/asm/parser.c
@@ -5,16 +5,20 @@
 ** Login   <nathan.schwarz@epitech.eu@epitech.net>
 **
 ** Started on  Fri Mar 24 17:05:13 2017 nathan
-** Last update Mon Mar 27 01:12:13 2017 nathan
+** Last update Mon Mar 27 01:46:17 2017 nathan
 */
 
 #include "mylib/define.h"
+#include "mylib/my_string.h"
 #include "assembly.h"
 #include "op.h"
 
 uint8_t	parser(char **file)
 {
   int	x;
+  int	y;
+  int	len;
+  int	len2;
 
   x = 2;
   while (file[x])
@@ -22,7 +26,20 @@ uint8_t	parser(char **file)
       if (file[x][0] == COMMENT_CHAR || file[x][0] == 0)
 	x++;
       else
-	printf("%s\n", file[x++]);
+	{
+	  y = 0;
+	  len = my_strlen(file[x]);
+	  while (y < 16)
+	    {
+	      len2 = my_strlen(op_tab[y].mnemonique);
+	      if (my_strcmp(op_tab[y].mnemonique, file[x]) == len - len2)
+		printf("%s\n", op_tab[y].mnemonique);
+	      y++;
+	    }
+	  if (y == 15)
+	    printf("%s\n", file[x]);
+	  x++;
+	}
     }
   return (SUCCESS);
 }
