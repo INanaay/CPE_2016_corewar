41a33d082e2f9a9881ea28b34c80eacdf906dc57
flavian gontier <flavian.gontier@epitech.eu>
2017-03-27 14:41:08 +0200

    [vm] useless

--- inc/bytecode.h
+++ inc/bytecode.h
@@ -5,7 +5,7 @@
 ** Login   <flavian.gontier@epitech.eu@epitech.net>
 ** 
 ** Started on  Sat Mar 25 11:41:43 2017 flavian gontier
-** Last update Sat Mar 25 12:08:34 2017 flavian gontier
+** Last update Mon Mar 27 14:40:56 2017 flavian gontier
 */
 
 #include <stdint.h>
@@ -21,6 +21,13 @@ typedef struct	s_header
   int32_t	size;
   int8_t	comment[COM_SIZE + 1];
 }
+
+typedef struct	s_instruct
+{
+  int8_t	id;
+  int8_t	param_type;
+  int8_t	params[REG_SIZE * 4];
+}		t_instruct;
 
 int	read_header(int fd, t_header *header);
 int	read_instruction_id(int fd, int *id);
