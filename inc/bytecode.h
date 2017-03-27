b5c15295f633d1c00395f6afbb3fcfa0a8a617fb
flavian gontier <flavian.gontier@epitech.eu>
2017-03-27 15:16:57 +0200

    [bytecode] stream structure updated

--- inc/bytecode.h
+++ inc/bytecode.h
@@ -5,7 +5,7 @@
 ** Login   <flavian.gontier@epitech.eu@epitech.net>
 ** 
 ** Started on  Sat Mar 25 11:41:43 2017 flavian gontier
-** Last update Sat Mar 25 12:08:34 2017 flavian gontier
+** Last update Mon Mar 27 15:16:43 2017 flavian gontier
 */
 
 #include <stdint.h>
@@ -20,7 +20,24 @@ typedef struct	s_header
   int8_t	name[NAME_SIZE + 1];
   int32_t	size;
   int8_t	comment[COM_SIZE + 1];
-}
+}		t_header;
+
+typedef struct	s_bigendian_stream
+{
+  int8_t	*data;
+  int32_t	data_count;
+  int32_t	position;
+}		t_bigendian_stream;
+
+/*
+** Stream functions
+*/
+void	init_stream(t_bigendian_stream *stream, int8_t *data, size_t n);
+int8_t	*reverse_bytes(int8_t *bytes, size_t n);
+int32_t	read_int32(t_bigendian_stream *stream);
+int16_t	read_int16(t_bigendian_stream *stream);
+int8_t	read_int8(t_bigendian_stream *stream);
+int8_t	*read_bytes(t_bigendian_stream *stream, size_t n);
 
 int	read_header(int fd, t_header *header);
 int	read_instruction_id(int fd, int *id);
