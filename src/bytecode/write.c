c3602c62f467b3d110d86027c645803b0939f342
Lebon Nathan <nathan.lebon@epitech.eu>
2017-03-27 19:19:19 +0200

    [bytecode] Write.c

--- src/bytecode/write.c
+++ src/bytecode/write.c
@@ -5,14 +5,20 @@
 ** Login   <nathan.lebon@epitech.eu>
 ** 
 ** Started on  Mon Mar 27 17:02:20 2017 NANAA
-** Last update Mon Mar 27 18:57:32 2017 NANAA
+** Last update Mon Mar 27 19:18:44 2017 NANAA
 */
 
-int8_t		write_bytes( int fd, const int8_t *bytes, size_t n)
+int8_t		write_bytes(int fd, const void *bytes, size_t n)
 {
-  if (fd == -1)
-    return (1);
-  if (write(fd, bytes, n) == -1)
-    return (1);
-  return (0);
+  int		count;
+  int8_t	*tmp;
+
+  if (fd <= 0)
+    return (EXIT_ERROR);
+  tmp = reverse_bytes(bytes, n);
+  count = write(fd, tmp, n);
+  free(tmp);
+  if (count != n)
+    return (EXIT_ERROR);
+  return (EXIT_SUCESS);
 }
