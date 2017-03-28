5ebb12c3c9c85a3219dad51dff1a3d113e8a242c
flavian gontier <flavian.gontier@epitech.eu>
2017-03-28 13:37:06 +0200

    [vm] include updated

--- inc/bytecode.h
+++ inc/bytecode.h
@@ -5,7 +5,7 @@
 ** Login   <flavian.gontier@epitech.eu@epitech.net>
 ** 
 ** Started on  Sat Mar 25 11:41:43 2017 flavian gontier
-** Last update Mon Mar 27 21:29:11 2017 flavian gontier
+** Last update Tue Mar 28 13:34:04 2017 flavian gontier
 */
 
 #include <stdint.h>
@@ -39,7 +39,7 @@ typedef struct	s_instruct
 /*
 ** Stream functions
 */
-t_stream	*init_stream(t_stream *stream, int8_t *data, size_t n);
+t_stream	*init_stream(int fd, t_stream *stream);
 int8_t		*reverse_bytes(int8_t *bytes, size_t n);
 int32_t		read_int32(t_stream *stream);
 int16_t		read_int16(t_stream *stream);
