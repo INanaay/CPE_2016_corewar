777b3306d8180d6da77f57d7a0683d0829dbe12c
Lebon Nathan <nathan.lebon@epitech.eu>
2017-03-29 20:23:51 +0200

    [bytecode] Fixed write header

--- src/bytecode/parsing/header.c
+++ src/bytecode/parsing/header.c
@@ -5,7 +5,7 @@
 ** Login   <flavian.gontier@epitech.eu@epitech.net>
 ** 
 ** Started on  Tue Mar 28 22:03:49 2017 flavian gontier
-** Last update Wed Mar 29 19:17:09 2017 NANAA
+** Last update Wed Mar 29 20:23:11 2017 NANAA
 */
 
 #include "bytecode.h"
@@ -18,13 +18,16 @@ void    read_header(t_stream *stream, t_header *header)
   read_bytes(stream, header->comment, sizeof(header->comment));
 }
 
-int8_t	write_header(int fd, const t_header *header)
+int8_t		write_header(int fd, const t_header *header)
 {
-  if (write_bytes(fd, &header->magic, sizeof(header->magic))
-      == EXIT_ERROR ||
-      write_bytes(fd, header->name, sizeof(header->name)) == EXIT_ERROR ||
-      write_bytes(fd, header->comment, sizeof(header->comment)) == EXIT_ERROR ||
-      write_bytes(fd, &header->size, sizeof(header->size)) == EXIT_ERROR)
+  int8_t   error;
+
+  error = 0;
+  error |= write_bytes(fd, &header->magic, sizeof(header->magic));
+  error |= write_bytes(fd, header->name, sizeof(header->name));
+  error |= write_bytes(fd, header->comment, sizeof(header->comment));
+  error |= write_bytes(fd, &header->size, sizeof(header->size));
+  if (error == 1)
     return (EXIT_ERROR);
   return (EXIT_SUCCESS);
 }
