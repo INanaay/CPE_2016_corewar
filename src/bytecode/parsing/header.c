8033e7b0c6c6f05fbf26e9211dcd28a4799c47ed
flavian gontier <flavian.gontier@epitech.eu>
2017-03-30 11:06:05 +0200

    [bytecode] compile errors (again)

--- src/bytecode/parsing/header.c
+++ src/bytecode/parsing/header.c
@@ -5,10 +5,11 @@
 ** Login   <flavian.gontier@epitech.eu@epitech.net>
 ** 
 ** Started on  Tue Mar 28 22:03:49 2017 flavian gontier
-** Last update Wed Mar 29 20:23:11 2017 NANAA
+** Last update Thu Mar 30 11:05:12 2017 flavian gontier
 */
 
 #include "bytecode.h"
+#include "libmy.h"
 
 void    read_header(t_stream *stream, t_header *header)
 {
