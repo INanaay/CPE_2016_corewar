c18ff5b262cbca49157d28d24b8f0fd6f90dc6ed
flavian gontier <flavian.gontier@epitech.eu>
2017-04-02 22:05:13 +0200

    [master] warnings

--- src/bytecode/io/utils.c
+++ src/bytecode/io/utils.c
@@ -5,19 +5,21 @@
 ** Login   <nathan.lebon@epitech.eu>
 ** 
 ** Started on  Mon Mar 27 16:49:59 2017 NANAA
-** Last update Thu Mar 30 21:07:33 2017 flavian gontier
+** Last update Sun Apr 02 22:04:56 2017 flavian gontier
 */
 
 #include <unistd.h>
 #include "bytecode.h"
 
-int8_t	*reverse_bytes(int8_t *bytes, size_t n)
+int8_t	*reverse_bytes(void *data, size_t n)
 {
   register int	index;
+  uint8_t	*bytes;
   uint8_t	*buffer;
   uint8_t	tmp;
 
   index = 0;
+  bytes = (uint8_t *)data;
   buffer = bytes;
   while (index < n / 2)
   {
