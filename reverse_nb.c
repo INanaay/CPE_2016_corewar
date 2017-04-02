a7cbed393e2358b20bdae52f177650f86ae5ff61
anatole zeyen <anatole.zeyen@epitech.eu>
2017-04-02 11:55:47 +0200

    [master]norme reverse_nb.c : parentheses et espaces

--- reverse_nb.c
+++ reverse_nb.c
@@ -1,11 +1,11 @@
 /*
 ** reverse_int.c for Project-Master in /home/NANAA/Projets/CPE_2016_corewar
-** 
+**
 ** Made by NANAA
 ** Login   <nathan.lebon@epitech.eu>
-** 
+**
 ** Started on  Sat Mar 25 14:05:18 2017 NANAA
-** Last update Sun Apr  2 11:49:47 2017 anatole zeyen
+** Last update Sun Apr  2 11:55:10 2017 anatole zeyen
 */
 #include <fcntl.h>
 
@@ -20,7 +20,7 @@ unsigned int		my_reverse_int(unsigned int nb)
   c2 = (nb >> 8) & 255;
   c3 = (nb >> 16) & 255;
   c4 = (nb >> 24) & 255;
-  return (c1 << 24) + (c2 << 16) + (c3 << 8) + c4;
+  return ((c1 << 24) + (c2 << 16) + (c3 << 8) + c4);
 }
 
 unsigned short		my_reverse_short(unsigned short nb)
@@ -30,5 +30,5 @@ unsigned short		my_reverse_short(unsigned short nb)
 
   c1 = nb & 255;
   c2 = (s >> 8) & 255;
-  return (c1 << 8) + c2;
+  return ((c1 << 8) + c2);
 }
